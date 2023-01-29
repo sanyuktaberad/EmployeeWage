f78f530f774277b667be1c2f70162f4fdcc8cf6b
sanyuktaberad <sanyukta.berad@gmail.com>
2023-01-29 16:24:40 +0530

    Added: Code To check employee attendence

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -3,5 +3,11 @@ package com.bridgelabz;
 public class EmployeeWage {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
+		double number = Math.floor(Math.random() * 10) % 2;
+		if (number == 1) {
+			System.out.println("Employee is Present");
+		} else {
+			System.out.println("Employee is Absent");
 		}
+	}
 }
