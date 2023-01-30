427531975b91b37659e41fa93ed5ba9c36d59512
sanyuktaberad <sanyukta.berad@gmail.com>
2023-01-30 10:45:19 +0530

    Added: Code for calculating wages for a month

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,18 +1,38 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class EmployeeWage {
-	
+
 	static int wagePerHour = 20;
-	static int workingHoursPerDay = 8;
-	static int wage = (int)(wagePerHour * workingHoursPerDay);
-	
+	static int workingHoursPerDayParttimer = 4;
+	static int workingHoursPerDayFulltimer = 8;
+	static int workingDaysForAMonth = 20;
+
+	static int wage = (int)(wagePerHour * workingHoursPerDayFulltimer * workingDaysForAMonth);
+	static int wage2 = (int)(wagePerHour * workingHoursPerDayParttimer * workingDaysForAMonth);
+
+
 	public static void main(String[] args) {
+
 		System.out.println("Welcome to Employee Wage Computation Program");
-		double number = Math.floor(Math.random() * 10) % 2;
-		if (number == 1) {
-			System.out.println("Employee is Present & its daily wage is : "+ wage +".");
-		} else {
+		System.out.println("Enter 1 if the employee is present & fulltimer; enter 2 if the employee is present & parttimer; enter 0 if the employee is absent. ");
+
+		Scanner scan = new Scanner(System.in); 
+		int number = scan.nextInt();
+
+		switch (number) {
+		case 1 :
+			System.out.println("Employee is Present & Fulltimer hence its monthly wage is : "+ wage +".");
+			break;
+
+		case 2 :  
+			System.out.println("Employee is Present &  Parttimer hence its monthly wage is : "+ wage2 +".");
+			break;
+
+		default:
 			System.out.println("Employee is Absent & its daily wage is 0.");
 		}
+		return;
 	}
 }
